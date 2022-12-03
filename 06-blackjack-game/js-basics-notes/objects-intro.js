260bd5ad69c8513972bbb12336252821e0241526
Amirah Nasihah <amirahnasihah97@gmail.com>
2022-12-03 15:37:14 +0800

    update objects-intro.js

--- 06-blackjack-game/js-basics-notes/objects-intro.js
+++ 06-blackjack-game/js-basics-notes/objects-intro.js
@@ -1,3 +1,8 @@
 // Objects -> ability to store data in-depth, a composite & complex data type.
 // key-value pairs.
 
+let course = {
+	title: "Learn CSS Grid for free",
+	lessons: 16,
+	creator: 
+}
