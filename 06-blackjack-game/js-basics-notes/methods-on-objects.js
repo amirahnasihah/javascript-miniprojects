26c46dc42e1895b494431676fad668943384711d
amirahever <amirahnasihah97@gmail.com>
2022-12-03 21:01:20 +0800

    methods on object

--- 06-blackjack-game/js-basics-notes/methods-on-objects.js
+++ 06-blackjack-game/js-basics-notes/methods-on-objects.js
@@ -1,83 +1,33 @@
-// Logic Blackjack: <21: happy | 21: partying | >21: sad
+// Methods On Objects.
+// observe the coding patterns in the game.
 
+// player is an Object
+// create a function inside of Object, something which we also call as Method
 let player = {
-	name: "Jane",
-	chips: 145
+  name: "Jane",
+  chips: 145,
+  sayHello: function () {
+    console.log("Hello World!");
+  },
 };
 
-let cards = []; // array - ordered list of items
-let sum = 0;
-let hasBlackJack = false; // initially, player dont have BlackJack
-let isAlive = false; // initially, player not yet starting
-let message = " ";
-let messageEl = document.getElementById("message-element");
-let sumEl = document.getElementById("sum-element");
-let cardEl = document.getElementById("card-element");
+// invoke the function inside the Object
+player.sayHello(); // Hello World!
 
-let playerEl = document.getElementById('player-el')
-playerEl.textContent = player.name + ': £' + player.chips
+// TL;DR
+// a variable - a dot - a function that we calling - with the parentheses at the end. 👇
+document.getElementById("message-element");
+document.getElementById("sum-element");
+document.getElementById("card-element");
+document.getElementById("player-el");
 
+// x
+playerEl.textContent = player.name + ": £" + player.chips;
+
+// Object with the Method
 console.log(cards); // to check if the card given earlier
 
-// Make this function return a random number between 1 and 13
-// if 1 (ace)     -> return 11 (worth 11)
-// if 11-13 -> return 10
-function getRandomCard() {
-	let randomNumber = Math.floor(Math.random() * 13) + 1;
+// Math is an Object, random() is a function in that Object also called a Method.
+Math.floor(Math.random() * 13) + 1;
 
-	if (randomNumber > 10) {
-		return 10;
-	} else if (randomNumber === 1) {
-		return 11;
-	} else {
-		return randomNumber;
-	}
-}
-
-const startGame = () => {
-	isAlive = true; // starting the game
-	// Generate two random numbers
-	let firstCard = getRandomCard();
-	let secondCard = getRandomCard();
-	// Re-assign the cards and sum variables so that the game can start
-
-	cards = [firstCard,
-		secondCard];
-	sum = firstCard + secondCard;
-	renderGame();
-};
-
-const renderGame = () => {
-	// render out firstCard and secondCard
-	sumEl.textContent = "Sum: " + sum;
-	// render out ALL the cards we have
-	cardEl.textContent = "Cards: ";
-	// a for loop to render all cards
-	for (let i = 0; i < cards.length; i++) {
-		cardEl.textContent += cards[i] + " ";
-	}
-
-	if (sum <= 20) {
-		message = "Do you want to draw a new card? 🙂";
-	} else if (sum === 21) {
-		message = "Wohoo! You've got Blackjack! 🥳";
-		hasBlackJack = true;
-	} else {
-		message = "You're out of the game! 😭";
-		isAlive = false;
-	}
-
-	// console.log(message);
-	messageEl.textContent = message;
-};
-
-const newCard = () => {
-	// Only allow the player to get a new card if she IS alive and does NOT have Blackjack
-	if (isAlive === true && hasBlackJack === false) {
-		console.log("Drawing a new card from the deck!");
-		let card = getRandomCard();
-		sum += card; // Add the new card to the sum variable
-		cards.push(card); // Push the card to the cards array
-		renderGame();
-	}
-};
+// Method is a function that attached to an Object
