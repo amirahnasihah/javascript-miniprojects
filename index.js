4f73c2769f1c5677b52c2ed37aee5d53cd5cb92a
amirahever <amirahnasihah97@gmail.com>
2022-12-10 00:10:25 +0800

    push value from input field

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 
-// Push the value "www.awesomelead.com" to myLeads when the input button is clicked
+// Push the value "www.awesomelead.com" to myLeads when the input button is clicked.
+// Google -> "get value from input field javascript"
 inputBtn.addEventListener("click", () => {
-  myLeads.push("www.awesomelead.com");
+  // myLeads.push("www.awesomelead.com");
+  myLeads.push(inputEl.value);
   console.log(myLeads);
 });
