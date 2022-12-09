a28cae7b425f760967e271116bfe08519297ac79
amirahever <amirahnasihah97@gmail.com>
2022-12-10 00:32:21 +0800

    render the leads in ul list

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
   console.log(myLeads);
 });
 
-// Log out ALL the items in the myLeads array using a for loop
 for (let i = 0; i < myLeads.length; i++) {
   const element = myLeads[i];
-  console.log(element);
+  // console.log(element);
+  ulEl.textContent += element + " ";
 }
