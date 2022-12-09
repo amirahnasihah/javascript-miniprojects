950396c2156b565453fd8c16854bed9cedeabdce
amirahever <amirahnasihah97@gmail.com>
2022-12-10 00:18:36 +0800

    forloop to log all array leads

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-let myLeads = [];
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 
-// Push the value "www.awesomelead.com" to myLeads when the input button is clicked.
-// Google -> "get value from input field javascript"
 inputBtn.addEventListener("click", () => {
-  // myLeads.push("www.awesomelead.com");
   myLeads.push(inputEl.value);
   console.log(myLeads);
 });
+
+// Log out ALL the items in the myLeads array using a for loop
+for (let i = 0; i < myLeads.length; i++) {
+  const element = myLeads[i];
+  console.log(element);
+}
