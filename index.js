116b92ac40868d05470cf45989bd448f34815c2c
amirahever <amirahnasihah97@gmail.com>
2022-12-11 21:55:34 +0800

    create renderLeads()

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  // console.log(myLeads);
+  // 2. Call the renderLeads() function
+  renderLeads();
 });
 
-let listItems = " ";
-
-for (let i = 0; i < myLeads.length; i++) {
-  listItems += "<li>" + myLeads[i] + "</li>";
-  // console.log(listItems);
-}
-ulEl.innerHTML = listItems;
+// 1. Wrap the code below in a renderLeads() function
+const renderLeads = () => {
+  let listItems = " ";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+    // console.log(listItems);
+  }
+  ulEl.innerHTML = listItems;
+};
