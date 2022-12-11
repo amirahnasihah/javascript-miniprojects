cc2bdab5ab5bf432d0fa6f3d93b3ab4c0f35b07e
amirahever <amirahnasihah97@gmail.com>
2022-12-11 22:46:07 +0800

    refactor

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
-  // console.log(myLeads);
-  // Clear out the input field when clicked save input button
   inputEl.value = "";
   renderLeads();
 });
@@ -14,8 +12,13 @@ inputBtn.addEventListener("click", () => {
 const renderLeads = () => {
   let listItems = " ";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += `<li><a target='_blank' href='${myLeads[i]}'> + myLeads[i] + </a></li>`;
-    // console.log(listItems);
+    listItems += `
+      <li>
+        <a target='_blank' href='${myLeads[i]}'> 
+            ${myLeads[i]} 
+        </a>
+      </li>
+    `;
   }
   ulEl.innerHTML = listItems;
 };
