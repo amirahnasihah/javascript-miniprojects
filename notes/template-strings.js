cf12d45df7fb18ef4f80ba2c7e2f727c0da904e4
amirahever <amirahnasihah97@gmail.com>
2022-12-11 22:41:11 +0800

    more on template strings

--- notes/template-strings.js
+++ notes/template-strings.js
@@ -1,9 +1,16 @@
 // practice template strings/literals
 
 const recipient = "James";
+const sender = "John Doe Mozart";
 
 // Refactor the email string to use template strings
-// const email = "Hey " + recipient + "! How is it going? Cheers Per";
-const email = `Hey ${recipient}! How is it going? Cheers Per`;
+// Break the email string into multiple lines
+const email = `Hey ${recipient}!
+How is it going? Cheers
+${sender}`;
 
-console.log(email); // Hey James! How is it going? Cheers Per
+console.log(email);
+
+// Hey James!
+// How is it going? Cheers
+// John Doe Mozart
