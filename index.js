2e78ca1af1f34e0b0516cdc986dd3b546d39a029
amirahever <amirahnasihah97@gmail.com>
2022-12-11 22:12:25 +0800

    a tag

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ inputBtn.addEventListener("click", () => {
 const renderLeads = () => {
   let listItems = " ";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+    listItems +=
+      "<li><a target='_blank' href='" +
+      myLeads[i] +
+      "'>" +
+      myLeads[i] +
+      "</a></li>";
     // console.log(listItems);
   }
   ulEl.innerHTML = listItems;
